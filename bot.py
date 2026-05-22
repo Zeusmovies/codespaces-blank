e98e8fadbf2fe61aa7b136ddc8bfc0d8c405764c
Zeusmovies <88440665+Zeusmovies@users.noreply.github.com>
2026-05-22 10:03:40 +0530

    Disable ping_server task on Heroku
    Comment out the ping_server task creation for Heroku.

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ async def dreamxbotz_start():
             sys.modules["plugins." + plugin_name] = load
             print("DreamxBotz Imported => " + plugin_name)
     if ON_HEROKU:
-        asyncio.create_task(ping_server()) 
+       # asyncio.create_task(ping_server()) 
     b_users, b_chats = await db.get_banned()
     temp.BANNED_USERS = b_users
     temp.BANNED_CHATS = b_chats
