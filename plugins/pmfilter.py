3cf9937cb1096d606dd9a8e54a1f0d5f8c18b13b
Zeusmovies <88440665+Zeusmovies@users.noreply.github.com>
2026-05-30 06:55:58 +0530

    Update pmfilter.py

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -2041,26 +2041,28 @@ async def advantage_spell_chok(client, message):
         r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
         "", message.text, flags=re.IGNORECASE)
     query = query.strip() + " movie"
-    
-    try:
-         movies = await get_poster(search, bulk=True)
-            
-         if not movies:
-             suggestion = await ai_spell_check(chat_id, search)
+try:
+        movies = await get_poster(search, bulk=True)
 
-             if suggestion:
-                 movies = await get_poster(suggestion, bulk=True)
+        if not movies:
+            suggestion = await ai_spell_check(chat_id, search)
 
-        except:
-               k = await
-               message.reply(script.I_CUDNT.format(message.from_user.mention))
-               await asyncio.sleep(60)
-               await k.delete()
-         return
+            if suggestion:
+                movies = await get_poster(suggestion, bulk=True)
 
-           try:
-               await message.delete()
-           except:
+    except Exception:
+        k = await message.reply(
+            script.I_CUDNT.format(message.from_user.mention)
+        )
+        await asyncio.sleep(60)
+        await k.delete()
+
+        try:
+            await message.delete()
+        except Exception:
+            pass
+
+        return
                pass
 
            return
