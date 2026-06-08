700750b45b9fe69717e3ab2077dd4abda551cb88
Zeusmovies <88440665+Zeusmovies@users.noreply.github.com>
2026-06-08 23:35:51 +0530

    Update info.py

--- info.py
+++ info.py
@@ -73,7 +73,7 @@ STAR_PREMIUM_PLANS = {
 # ============================
 # MongoDB Configuration
 # ============================
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://zeusmovies:zeus123@cluster0.m7kwkra.mongodb.net/?retryWrites=true&w=majority")  # MongoDB URI for the database
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://zeusbot:Zeus12345@database001.xfoax6g.mongodb.net/?appName=DATABASE001")  # MongoDB URI for the database
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0") # Database name (default: cluster)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'royal_files') # Collection name (default: dreamcinezone_files)
 
